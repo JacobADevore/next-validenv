b8cffe0a6c70f049af1b95462a29e39aa5c4d56f
Jacob Devore <jacob@blient.com>
2023-01-14 02:05:46 -0500

    Next.js example

---
+++ example/env.mjs
@@ -0,0 +1,27 @@
+// @ts-check
+import { validateEnvironmentVariables } from "next-validenv";
+import { z } from "zod";
+
+/**
+ * Specify your server-side environment variables schema here.
+ * This way you can ensure the app isn't built with invalid env vars.
+ */
+export const serverSchema = z.object({
+  NODE_ENV: z.enum(["development", "test", "production"]),
+});
+
+/**
+ * Specify your client-side environment variables schema here.
+ * This way you can ensure the app isn't built with invalid env vars.
+ * To expose them to the client, prefix them with `NEXT_PUBLIC_`.
+ */
+export const clientSchema = z.object({
+  // NEXT_PUBLIC_CLIENT: z.string(),
+});
+
+const environmentVariables = validateEnvironmentVariables(
+  clientSchema,
+  serverSchema
+);
+
+export const env = environmentVariables;
