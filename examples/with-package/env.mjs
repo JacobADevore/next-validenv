511d8dcc0df81395f7496f1553d892bba380cd64
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2023-01-15 15:28:02 +0100

    failing test

--- examples/with-package/env.mjs
+++ examples/with-package/env.mjs
@@ -8,6 +8,17 @@ import { z } from "zod";
  */
 export const serverSchema = z.object({
   NODE_ENV: z.enum(["development", "test", "production"]),
+  NEXTAUTH_SECRET:
+    process.env.NODE_ENV === "production" // <-- THIS BREAKS
+      ? z.string().min(1)
+      : z.string().min(1).optional(),
+  NEXTAUTH_URL: z.preprocess(
+    // This makes Vercel deployments not fail if you don't set NEXTAUTH_URL
+    // Since NextAuth.js automatically uses the VERCEL_URL if present.
+    (str) => process.env.VERCEL_URL ?? str, // <-- THIS BREAKS.
+    // VERCEL_URL doesn't include `https` so it cant be validated as a URL
+    process.env.VERCEL ? z.string() : z.string().url() // <-- THIS BREAKS.
+  ),
 });
 
 /**
