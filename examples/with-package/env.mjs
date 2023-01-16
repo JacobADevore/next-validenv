66714f3f75cc12f3799c6089d8be39bba01f87d9
Jacob Devore <jacob@blient.com>
2023-01-15 22:30:32 -0500

    update examples

--- examples/with-package/env.mjs
+++ examples/with-package/env.mjs
@@ -1,28 +1,26 @@
 //@ts-check
 import { validateEnvironmentVariables } from "next-validenv";
+
 import { z } from "zod";
 
 /**
- * Specify your server-side environment variables schema here.
- * This way you can ensure the app isn't built with invalid env vars.
- * @template {ReturnType<typeof z.object>} T
- * @param {T} serverSchema
- * @returns {z.infer<T>}
+ * Specify your environment variables schema here.
+ * This way, you can ensure the app isn't built with invalid environment variables.
+ * By default, environment variables are only available in the Node.js environment, meaning they won't be exposed to the browser. In order to expose a variable to the browser you have to prefix the variable with NEXT_PUBLIC_.
+ * -> Don't use any Zod .transform() methods in the schema (will cause `implicitly has type 'any'` error) <-
  */
-export const serverSchema = z.object({
+export const schema = z.object({
   NODE_ENV: z.enum(["development", "test", "production"]),
-});
-
-/**
- * Specify your client-side environment variables schema here.
- * This way you can ensure the app isn't built with invalid env vars.
- * To expose them to the client, prefix them with `NEXT_PUBLIC_`.
- * @template {ReturnType<typeof z.object>} T
- * @param {T} serverSchema
- * @returns {z.infer<T>}
- */
-export const clientSchema = z.object({
   NEXT_PUBLIC_TEST: z.string(),
 });
 
-export const env = validateEnvironmentVariables(serverSchema, clientSchema);
+/**
+ * Environment variable declarations based on the schema help structure your environment variables programmatically.
+ * @type {{ [k in keyof z.infer<typeof schema>]: z.infer<typeof schema>[k] | undefined }}
+ */
+export const env = {
+  NODE_ENV: process.env.NODE_ENV,
+  NEXT_PUBLIC_TEST: process.env.NEXT_PUBLIC_TEST,
+};
+
+validateEnvironmentVariables(schema, env);
